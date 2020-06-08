31466a519411ec784cd8836fc12869255165c33c
Roberto Orozco <issc.roberto@outlook.com>
2020-06-07 19:51:06 -0500

    Add app insights

--- Karaoke/Server/Startup.cs
+++ Karaoke/Server/Startup.cs
@@ -25,6 +25,7 @@ namespace Karaoke.Server
             services.AddRazorPages();
 
             services.AddScoped(typeof(SongService), typeof(SongService));
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
